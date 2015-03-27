21ebfa0b6c29778462d39e9482938f800dfdf4eb
sheepmansLab <tsu23hiroc@msn.com>
2015-03-28 02:49:32 +0900

    replace commonlib.jar

---
+++ CommonLib/src/jp/sheepman/common/fragment/BaseDialogFragment.java
@@ -0,0 +1,7 @@
+package jp.sheepman.common.fragment;
+
+import android.app.DialogFragment;
+
+public abstract class BaseDialogFragment extends DialogFragment {
+	public abstract void callback(); 
+}
